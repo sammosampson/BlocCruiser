5ae89c051a5c0bf84c985fb6bdac84a14e613be0
sammosampson <sammosampson@gmail.com>
2014-06-09 17:33:34 +0100

    More

--- BlocCrusier/GameMessenger.cs
+++ BlocCrusier/GameMessenger.cs
@@ -1,4 +1,5 @@
 using System;
+using SystemDot.Messaging.Handling.Actions;
 using SystemDot.Messaging.Simple;
 using BlocCrusier.ObjectPooling;
 
@@ -12,6 +13,12 @@ namespace BlocCrusier
             onCreation(message);
             Messenger.Send(message, groupId);
             ObjectPool.Return(message);
+        }
+
+        public static ActionSubscriptionToken<T> RegisterHandler<T>(EntityIdentifier groupId, Action<T> toRegister) 
+            where T : new()
+        {
+            return Messenger.RegisterHandler(toRegister, groupId);
         }
     }
 }
--- BlocCrusier/Player.cs
+++ BlocCrusier/Player.cs
@@ -1,13 +1,14 @@
-using Android.Content;
 using BlocCrusier.Drawing;
+using BlocCrusier.Physics;
 using Cocos2D;
 
 namespace BlocCrusier
 {
-    public class Player : Entity<PlayerEntityIdentifier>
+    public class Player : CCNode, Entity<PlayerEntityIdentifier>
     {
         public Player()
         {
+            AddChild(new DynamicBody<PlayerEntityIdentifier>(GetIdentifier()));
             Position = CCDirector.SharedDirector.WinSize.Center;
         }
 
@@ -18,9 +19,42 @@ namespace BlocCrusier
             CCDrawingPrimitives.End();
             base.Draw();
         }
+
+        public PlayerEntityIdentifier GetIdentifier()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+
+    public class DynamicBody<TEntityIdentifier> : CCNode, Entity<EntityIdentifier>
+        where TEntityIdentifier : EntityIdentifier
+    {
+        readonly TEntityIdentifier identifier;
+
+        public DynamicBody(TEntityIdentifier identifier)
+        {
+            this.identifier = identifier;
+            GameMessenger.RegisterHandler<PhysicsBodyMoved>(GetIdentifier(), OnPhysicsBodyMoved);
+        }
+
+        void OnPhysicsBodyMoved(PhysicsBodyMoved @event)
+        {
+            
+        }
+
+        public EntityIdentifier GetIdentifier()
+        {
+            return identifier;
+        }
     }
 
     public struct PlayerEntityIdentifier : EntityIdentifier
     {
+    }
+
+    public interface Entity<out TEntityIdentifier> 
+        where TEntityIdentifier : EntityIdentifier
+    {
+        TEntityIdentifier GetIdentifier();
     }
 }
